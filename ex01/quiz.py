2ed08f9eed549ae1c7ab5e3c0c85d7ff5d97bde9
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-11-22 15:43:50 +0900

    練習問題のクイズ作成

---
+++ ex01/quiz.py
@@ -0,0 +1,24 @@
+import random
+
+def shutudai():
+    que_dct = {"サザエの旦那の名前は？" : ("マスオ", "ますお"),
+                "カツオの妹の名前は？" : ("ワカメ", "わかめ"),
+                "タラオはカツオから見てどんな関係？" : ("甥", "おい", "甥っ子", "おいっこ")}
+    que = random.choice(["サザエの旦那の名前は？", "カツオの妹の名前は？", "タラオはカツオから見てどんな関係？"])
+    print(que)
+    return que_dct[que]
+
+
+
+def kaitou(anss, usr):
+    if usr in anss:
+        print("正解!!!")
+    else:
+        print("出直してこい")
+
+
+
+if __name__ == "__main__":
+    anss = shutudai()
+    usr = input("答えるんだ：")
+    kaitou(anss, usr)
