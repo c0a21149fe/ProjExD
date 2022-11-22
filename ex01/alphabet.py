a2ce142408233d57507c7c24a5f0c82af64a513d
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-11-22 17:18:51 +0900

    あと実行時間の計測だけ

---
+++ ex01/alphabet.py
@@ -0,0 +1,47 @@
+import random
+
+len_all = 10
+len_lost = 3
+alphabet = [chr(i) for i in range(65, 91)] #AtoZ
+alph_list = random.sample(alphabet, len_all)
+
+def start():
+    def_lst = alph_list.copy()
+    while(True):
+        cur_lst = random.sample(def_lst, len(def_lst))
+        alph = " ".join(cur_lst.copy())
+        lost_lst = [""] * len_lost
+        for i in range(len_lost):
+            lost_lst[i] = cur_lst.pop(random.randint(0, len_all-1-i))
+        cur = " ".join(cur_lst)
+        lost = " ".join(lost_lst)
+        print(f"対象文字：\n{alph}")
+        print(f"欠損文字：\n{lost}")
+        print(f"表示文字：\n{cur}")
+        
+        try:
+            ans1 = int(input("欠損文字はいくつあるでしょうか？："))
+        except ValueError:
+            print("数字を入力してください.最初からやり直します")
+            print("-"*40)
+            continue
+        if ans1 == len_lost:
+            print("正解です。それでは, 具体的に欠損文字を１つずつ入力してください。")
+            for i in range(len_lost):
+                ans2 = input(f"{i+1}つ目の文字を入力してください：")
+                if ans2 in lost_lst:
+                    lost_lst.remove(ans2)
+                else:
+                    print("不正解です.また、チャレンジしてください")
+                    break
+            else:
+                print("全問正解です")
+                break
+        else:
+            print("不正解です.また、チャレンジしてください")
+        print("-" * 40)
+            
+
+if __name__ == "__main__":
+    start()
+
