4e6f6280ed173b67923a62454db366eb893dfad7
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-11-29 14:26:36 +0900

    練習2

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,6 +7,16 @@ root = tk.Tk()
 root.title("tk")
 root.geometry("300x500")
 
+r, c = 0, 0
+for i in range(9, -1, -1):
 
+    num_key = tk.Button(root, text=i, width=4, height=2, font=("", 30))
+    num_key.grid(row=r, column=c)
+    
+    if i % 3 == 1:
+        r += 1
+        c = 0
+    else:
+        c += 1
 
 root.mainloop()
