eace1a851ffcf8003e27a3075d6ca189790e2a11
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-11-29 15:53:41 +0900

    必要なすべてのキーの表示, この後機能を実装

--- ex02/calc.py
+++ ex02/calc.py
@@ -15,15 +15,27 @@ def button_click(event):
     else: #=以外のボタン
         entry.insert(tk.END, num)
 
+
+
 root = tk.Tk()
 root.title("tk")
-root.geometry("300x500")
+root.geometry("400x600")
 
-entry = tk.Entry(root, justify="right", width=10, font=("", 40))
-entry.grid(row=0, column=0, columnspan=3)
+entry = tk.Entry(root, justify="right", width=14, font=("", 40))
+entry.grid(row=0, column=0, columnspan=4)
 
 
 r, c = 1, 0
+funcs = ["↶", "↷", "√", "C"]
+for func in funcs:
+    func_key = tk.Button(root, text = func, width=4, height=2, font=("", 30),
+                         background="#dcdcdc")
+    func_key.grid(row=r, column=c)
+    func_key.bind("<1>", button_click)
+    c += 1
+
+
+r, c = 2, 0
 for i in range(9, -1, -1):
 
     num_key = tk.Button(root, text=i, width=4, height=2, font=("", 30))
@@ -32,18 +44,24 @@ for i in range(9, -1, -1):
     if i % 3 == 1:
         r += 1
         c = 0
+        if i == 1:
+            c = 1
     else:
         c += 1
+    print(c)
 
-operators = ["+", "="]
+r, c = 2, 3
+operators = ["*", "-", "+", "="]
 for ope in operators:
-    button = tk.Button(root, text=ope, width=4, height=2, font=("", 30))
+    if ope != "=":
+        button = tk.Button(root, text=ope, width=4, height=2,
+                        font=("", 30), background="#dcdcdc")
+    else:
+        button = tk.Button(root, text=ope, width=4, height=2,
+                           font=("", 30), background="#40e0d0")
     button.grid(row = r, column=c)
     button.bind("<1>", button_click)
-    if i % 3 == 1:
-        r += 1
-        c = 0
-    else:
-        c += 1
+    r += 1
+
 
 root.mainloop()
