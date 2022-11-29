4dc3b3cd754e067cd12362cba38cec386a6e8ed7
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-11-29 14:39:38 +0900

    練習5

--- ex02/calc.py
+++ ex02/calc.py
@@ -27,4 +27,15 @@ for i in range(9, -1, -1):
     else:
         c += 1
 
+operators = ["+", "="]
+for ope in operators:
+    button = tk.Button(root, text=ope, width=4, height=2, font=("", 30))
+    button.grid(row = r, column=c)
+    button.bind("<1>", button_click)
+    if i % 3 == 1:
+        r += 1
+        c = 0
+    else:
+        c += 1
+
 root.mainloop()
