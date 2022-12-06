3cde1b6f29dfe6c163460c555b4e01584c524eca
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-12-06 14:23:52 +0900

    3番

--- ex03/maze.py
+++ ex03/maze.py
@@ -9,4 +9,9 @@ root.title("迷えるこうかとん")
 canvas = tk.Canvas(root, width=1500, height=800, bg="black")
 canvas.pack()
 
+koukaton = tk.PhotoImage(file="fig/8.png")
+cx, cy = 300, 400
+canvas.create_image(cx, cy, image=koukaton, tag="koukaton")
+canvas.pack()
+
 root.mainloop()
