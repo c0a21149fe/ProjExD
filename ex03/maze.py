b8f8406b0adb6c63618d8ed5cf1ea1a13f66106c
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-12-06 17:38:54 +0900

    修正しました。修正後、動作確認済み。 #7

--- ex03/maze.py
+++ ex03/maze.py
@@ -47,46 +47,46 @@ def main_proc(): #メイン処理（繰り返し）
         jid = root.after(100, main_proc) #100ms毎に再帰
 
 
+if __name__ == "__main__":
+    root = tk.Tk() #ウィンドウ作成
+    root.title("迷えるこうかとん")
+    #root.geometry("1500x900")
 
-root = tk.Tk() #ウィンドウ作成
-root.title("迷えるこうかとん")
-#root.geometry("1500x900")
-
-canvas = tk.Canvas(root, width=1500, height=800, bg="black")
-canvas.pack()
-
-
-maze_lst = mm.make_maze(15, 9) #
-mm.show_maze(canvas, maze_lst)
-
-#ゴールの表示
-goal = random.choice([(1, 7), (13, 1), (13, 7)])
-goal_x, goal_y = goal
-canvas.create_rectangle(goal_x*100, goal_y*100, goal_x *
-                        100+100, goal_y*100+100, fill="red")
-canvas.pack()
-maze_lst[goal_x][goal_y] = 2 #ゴールの番号
+    canvas = tk.Canvas(root, width=1500, height=800, bg="black")
+    canvas.pack()
 
 
-#迷路の座標
-mx, my = 1, 1
-#tkインター上の座標
-cx, cy = mx*100+50, my*100+50
-#こうかとんの表示
-#イメージを変数に代入
-koukaton = tk.PhotoImage(file="fig/8.png")
-koukaton_right = tk.PhotoImage(file="fig/8_right.png")
-koukaton_up = tk.PhotoImage(file="fig/8_up.png")
-koukaton_down = tk.PhotoImage(file="fig/8_down.png")
-#イメージの作成
-image_id = canvas.create_image(cx, cy, image=koukaton, tag="koukaton") #画像差し替えのためのidをimage_idに格納
-canvas.pack()
+    maze_lst = mm.make_maze(15, 9) #
+    mm.show_maze(canvas, maze_lst)
 
-key = ""
-root.bind("<KeyPress>", key_down)
-root.bind("<KeyRelease>", key_up)
+    #ゴールの表示
+    goal = random.choice([(1, 7), (13, 1), (13, 7)])
+    goal_x, goal_y = goal
+    canvas.create_rectangle(goal_x*100, goal_y*100, goal_x *
+                            100+100, goal_y*100+100, fill="red")
+    canvas.pack()
+    maze_lst[goal_x][goal_y] = 2 #ゴールの番号
 
-jid = None #ジョブIDの初期化
-main_proc()
 
-root.mainloop()
+    #迷路の座標
+    mx, my = 1, 1
+    #tkインター上の座標
+    cx, cy = mx*100+50, my*100+50
+    #こうかとんの表示
+    #イメージを変数に代入
+    koukaton = tk.PhotoImage(file="fig/8.png")
+    koukaton_right = tk.PhotoImage(file="fig/8_right.png")
+    koukaton_up = tk.PhotoImage(file="fig/8_up.png")
+    koukaton_down = tk.PhotoImage(file="fig/8_down.png")
+    #イメージの作成
+    image_id = canvas.create_image(cx, cy, image=koukaton, tag="koukaton") #画像差し替えのためのidをimage_idに格納
+    canvas.pack()
+
+    key = ""
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+
+    jid = None #ジョブIDの初期化
+    main_proc()
+
+    root.mainloop()
