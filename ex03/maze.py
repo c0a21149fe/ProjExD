12f271c04410e389cacac2a0f5d3ea9fe7ac77c3
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-12-06 14:33:34 +0900

    5番, 6番

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,6 +2,14 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
 root = tk.Tk()
 root.title("迷えるこうかとん")
 #root.geometry("1500x900")
@@ -15,4 +23,7 @@ canvas.create_image(cx, cy, image=koukaton, tag="koukaton")
 canvas.pack()
 
 key = ""
+root.bind("<KeyPress>", key_down)
+root.bind("<KeyRelease>", key_up)
+
 root.mainloop()
