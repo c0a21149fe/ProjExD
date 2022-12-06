1e88d5d187e036e1dc4dc60f85a9723bbf912555
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-12-06 14:15:53 +0900

    2番

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,7 +4,9 @@ import tkinter.messagebox as tkm
 
 root = tk.Tk()
 root.title("迷えるこうかとん")
-#root.geometry("500x200")
+#root.geometry("1500x900")
 
+canvas = tk.Canvas(root, width=1500, height=800, bg="black")
+canvas.pack()
 
 root.mainloop()
