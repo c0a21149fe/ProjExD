3f806d040ae8b85a15c30462ddcf484a755bff0a
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-12-13 14:20:16 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -5,10 +5,17 @@ import sys
 def main():
     clock = pg.time.Clock()
     pg.display.set_caption("逃げろこうかとん")  # タイトルバーに「逃げろこうかとん」と表示する
-    scrn_sfc = pg.display.set_mode((1000, 600)) #メインのsurfaceを設定
+    scrn_sfc = pg.display.set_mode((1600, 900)) #メインのsurfaceを設定
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg") #背景画像をロード
-    pgbg_rct = pgbg_sfc.get_rect()
-    scrn_sfc.blit(pgbg_sfc, pgbg_rct)
+    pgbg_rct = pgbg_sfc.get_rect() #背景rect
+    scrn_sfc.blit(pgbg_sfc, pgbg_rct) #背景をblit
+
+    #こうかとん画像の表示
+    tori_sfc = pg.image.load("fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+    scrn_sfc.blit(tori_sfc, tori_rct)
 
     while True:
         for event in pg.event.get():
