bd3ce4d9c24d4f9f8673535c9fc61f896f9f4773
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-12-13 14:45:44 +0900

    練習5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,11 +1,13 @@
 import pygame as pg
 import sys
+import random
 
 
 def main():
     clock = pg.time.Clock()
     pg.display.set_caption("逃げろこうかとん")  # タイトルバーに「逃げろこうかとん」と表示する
     scrn_sfc = pg.display.set_mode((1600, 900)) #メインのsurfaceを設定
+    scrn_rct = scrn_sfc.get_rect()
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg") #背景画像をロード
     pgbg_rct = pgbg_sfc.get_rect() #背景rect
     scrn_sfc.blit(pgbg_sfc, pgbg_rct) #背景をblit
@@ -17,10 +19,35 @@ def main():
     tori_rct.center = 900, 400
     scrn_sfc.blit(tori_sfc, tori_rct)
 
+    #爆弾の表示
+    bomb_sfc = pg.Surface((20, 20))
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc, bomb_rct)
+
     while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct)  # 背景をblit
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
+
+        #こうかとんを移動させる        
+        key_dct = pg.key.get_pressed()
+        if key_dct[pg.K_UP]:
+            tori_rct.move_ip(0, -1)
+        if key_dct[pg.K_DOWN]:
+            tori_rct.move_ip(0, 1)
+        if key_dct[pg.K_LEFT]:
+            tori_rct.move_ip(-1, 0)
+        if key_dct[pg.K_RIGHT]:
+            tori_rct.move_ip(1, 0)
+        
+        scrn_sfc.blit(tori_sfc, tori_rct)
 
         pg.display.update()
         clock.tick(1000)
