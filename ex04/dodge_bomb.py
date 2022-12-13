6193ba589ce84352476a1d1794ba7b9fcb662cf0
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-12-13 15:23:42 +0900

    練習7

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,6 +2,15 @@ import pygame as pg
 import sys
 import random
 
+#画面外に出たかどうかを判定する関数
+def check_bound(obj_rct, scr_rct):
+    yoko, tate = +1, +1
+    if obj_rct.left <= scr_rct.left or obj_rct.right >= scr_rct.right:
+        yoko = -1
+    if obj_rct.top <= scr_rct.top or obj_rct.bottom >= scr_rct.bottom:
+        tate = -1
+    return tate, yoko
+
 
 def main():
     clock = pg.time.Clock()
@@ -27,14 +36,16 @@ def main():
     bomb_rct.centerx = random.randint(0, scrn_rct.width)
     bomb_rct.centery = random.randint(0, scrn_rct.height)
     scrn_sfc.blit(bomb_sfc, bomb_rct)
+    vx, vy = +1, +1 #爆弾の速度を設定
 
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct)  # 背景をblit
+        
+        #×を押したら、ループから抜ける
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
-        scrn_sfc.blit(bomb_sfc, bomb_rct)
 
         #こうかとんを移動させる        
         key_dct = pg.key.get_pressed()
@@ -46,8 +57,27 @@ def main():
             tori_rct.move_ip(-1, 0)
         if key_dct[pg.K_RIGHT]:
             tori_rct.move_ip(1, 0)
+
+        #どこかしらはみ出ていたら
+        if check_bound(tori_rct, scrn_rct) != (+1, +1):
+            if key_dct[pg.K_UP]:
+                tori_rct.move_ip(0, 1)
+            if key_dct[pg.K_DOWN]:
+                tori_rct.move_ip(0, -1)
+            if key_dct[pg.K_LEFT]:
+                tori_rct.move_ip(1, 0)
+            if key_dct[pg.K_RIGHT]:
+                tori_rct.move_ip(-1, 0)
         
         scrn_sfc.blit(tori_sfc, tori_rct)
+
+        #爆弾の移動
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= tate
+        vy *= yoko
+        bomb_rct.move_ip(vx, vy)
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
+        
 
         pg.display.update()
         clock.tick(1000)
