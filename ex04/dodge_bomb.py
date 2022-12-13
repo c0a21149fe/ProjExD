ce8f3c568202f643393996602f97006e95c3cc2e
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-12-13 14:16:51 +0900

    練習2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,9 +3,20 @@ import sys
 
 
 def main():
+    clock = pg.time.Clock()
     pg.display.set_caption("逃げろこうかとん")  # タイトルバーに「逃げろこうかとん」と表示する
-    scrn_sfc = pg.display.set_mode((1600, 900))
-    pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
+    scrn_sfc = pg.display.set_mode((1000, 600)) #メインのsurfaceを設定
+    pgbg_sfc = pg.image.load("fig/pg_bg.jpg") #背景画像をロード
+    pgbg_rct = pgbg_sfc.get_rect()
+    scrn_sfc.blit(pgbg_sfc, pgbg_rct)
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        pg.display.update()
+        clock.tick(1000)
 
 
 
