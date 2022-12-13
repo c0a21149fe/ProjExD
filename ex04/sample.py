f3321d3b367b8fab9eaecc6a29c3b0bf8b71d9ab
C0A21149 <c0a21149fe@edu.teu.ac.jp>
2022-12-13 13:44:28 +0900

    こうかとん追加前

--- ex04/sample.py
+++ ex04/sample.py
@@ -1,9 +1,12 @@
 import pygame as pg
 import sys
 
-def main():
-    pass
 
+def main():
+    pg.display.set_caption("初めてのpygame") #タイトルバーに「初めてのpygame」と表示する
+    scrn_sfc = pg.display.set_mode((800, 600))
+    
+    pg.image.load("fig/6.png")
 if __name__ == "__main__":
     pg.init()
     main()
